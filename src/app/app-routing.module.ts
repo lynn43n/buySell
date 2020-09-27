058509a9499ba07a3f3cbf4e7d09f0587323b31c
lynn <nabulsylynn@gmail.com>
2020-09-28 02:47:02 +0300

    starting angular listing pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ListingsDetailPageComponent } from './listings-detail-page/listings-detail-page.component';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { MylistingsPageComponent } from './mylistings-page/mylistings-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/listings', pathMatch:'full'},
+  {path: 'listings', component: ListingsPageComponent, pathMatch:'full'},
+  {path: 'contact/:id', component: ContactPageComponent},
+  {path: 'edit-listing/:id', component: EditListingPageComponent},
+  {path: 'listing/:id', component: ListingsDetailPageComponent },
+  {path:'mylistings', component:MylistingsPageComponent},
+  {path: 'new-listing', component:NewListingPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/listings-detail-page/listings-detail-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listings-detail-page',
+  templateUrl: './listings-detail-page.component.html',
+  styleUrls: ['./listings-detail-page.component.css']
+})
+export class ListingsDetailPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
