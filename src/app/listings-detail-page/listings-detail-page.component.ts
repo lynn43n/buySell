5df0baa594ac01b4f2d634e541b653c01fdce3d4
lynn <nabulsylynn@gmail.com>
2020-09-28 14:36:25 +0300

    listing pag and detail page

--- src/app/listings-detail-page/listings-detail-page.component.ts
+++ src/app/listings-detail-page/listings-detail-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Listing } from '../types';
+import {fakeListings} from '../fake-data';
 
 @Component({
   selector: 'app-listings-detail-page',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listings-detail-page.component.css']
 })
 export class ListingsDetailPageComponent implements OnInit {
+  listing: Listing;
 
-  constructor() { }
+  constructor(
+    private route : ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    const id= this.route.snapshot.paramMap.get('id');
+    this.listing= fakeListings.find(l => l.id === id);
   }
 
 }
